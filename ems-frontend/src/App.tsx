382e3879421921d2dd4e164e0b74bf46af5919e1
@ismail <ismail.gems@mopa.gov.bd>
2025-06-15 14:42:46 +0600

    comnent outed

--- ems-frontend/src/App.tsx
+++ ems-frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Emp from "./components";
 import Header from "./components/Header";
-// import SideBar from "./components/SideBar";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
